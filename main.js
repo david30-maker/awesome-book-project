1b2df3bb2bbf24b4d8c356ab209b4665d4bc8f24
david30-maker <95672514+david30-maker@users.noreply.github.com>
2023-02-20 21:43:09 +0100

    Fix linters errors

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
-/*const isTitle = document.getElementById("#first-name");
+/* const isTitle = document.getElementById("#first-name");
 const isAuthor = document.getElementById("#first-name");
 const isButton = document.getElementById(".button-form")
-
-
 
 window.onload = () => {
 const  titleWork = [
@@ -13,7 +11,6 @@ const  titleWork = [
 
     },
 
-
  {
         title: "Gana book",
         author: "Art work",
@@ -23,33 +20,28 @@ const  titleWork = [
 ];
 
 const classGroup = `  <div class="booksAdded">
-    <p class="p-1"></p> 
+    <p class="p-1"></p>
     <p class="p-1">Art Work</p>
     <button class="removeBtn" onclick="Display.removeBook()">Remove</button>
     <hr>
-    </div> 
+    </div>
     </div>`
-
-
-
-
-
 
 }
 */
-const form = document.querySelector(".form");
-const titleInput = document.querySelector("#first-name");
-const authorInput = document.querySelector("#last-name");
-const book = document.querySelector(".books");
+const form = document.querySelector('.form');
+const titleInput = document.querySelector('#first-name');
+const authorInput = document.querySelector('#last-name');
+const book = document.querySelector('.books');
 let books;
 
 const displayBooks = () => {
-  if (localStorage.getItem("books") == null) {
+  if (localStorage.getItem('books') == null) {
     books = [];
   } else {
-    books = JSON.parse(localStorage.getItem("books"));
+    books = JSON.parse(localStorage.getItem('books'));
   }
-  let display = "";
+  let display = '';
   books.forEach((book, i) => {
     display += `
         <div class="booksAdded">
@@ -64,34 +56,34 @@ const displayBooks = () => {
 };
 // Supreeti, uncomment this part and push it back
 /*const addBooks = (Title, Author) => {
-  if (Title !== "" && Author !== "") {
+  if (Title !== '' && Author !== '') {
     books.push({ title: Title, author: Author });
-    localStorage.setItem("books", JSON.stringify(books));
+    localStorage.setItem('books', JSON.stringify(books));
   }
   displayBooks();
 };
 
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener('DOMContentLoaded', () => {
   displayBooks();
-}); */
+});
 
-form.addEventListener("submit", (evt) => {
+form.addEventListener('submit', (evt) => {
   evt.preventDefault();
   const titleValue = titleInput.value;
   const authorValue = authorInput.value;
   addBooks(titleValue, authorValue);
-});
+}); */
 
 /* eslint-disable no-unused-vars */
 // Badru, uncomment this part and push it
 /*const removeBook = (id) => {
-  if (localStorage.getItem("books") == null) {
+  if (localStorage.getItem('books') == null) {
     books = [];
   } else {
-    books = JSON.parse(localStorage.getItem("books"));
+    books = JSON.parse(localStorage.getItem('books'));
   }
   const bookIndex = books.findIndex((item, i) => i === id);
   books.splice(bookIndex, 1);
-  localStorage.setItem("books", JSON.stringify(books));
+  localStorage.setItem('books', JSON.stringify(books));
   displayBooks();
 }; */
